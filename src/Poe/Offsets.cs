b9ae1ee4111ea48f8c9a8984a82ba03432628e6c
tehcheat <thecheatuc@gmail.com>
2016-03-13 14:36:50 -0400

    Updated Offsets for Steam.

--- src/Poe/Offsets.cs
+++ src/Poe/Offsets.cs
@@ -6,7 +6,7 @@ namespace PoeHUD.Poe
     public class Offsets
     {
         public static Offsets Regular = new Offsets { IgsOffset = 0, IgsDelta = 0, ExeName = "PathOfExile" };
-        public static Offsets Steam = new Offsets { IgsOffset = 24, IgsDelta = 0, ExeName = "PathOfExileSteam" };
+        public static Offsets Steam = new Offsets { IgsOffset = 0x1C, IgsDelta = 0x4, ExeName = "PathOfExileSteam" };
         /* offsets from some older steam version:
 		 	Base = 8841968;
 			FileRoot = 8820476;
@@ -134,7 +134,7 @@ namespace PoeHUD.Poe
         public int IgsOffset { get; private set; }
         //public int InGameOffset { get; private set; }
 
-        public int IgsOffsetDelta => IgsOffset - IgsDelta;
+        public int IgsOffsetDelta => IgsOffset + IgsDelta;
 
         public void DoPatternScans(Memory m)
         {
--- src/Poe/RemoteMemoryObjects/IngameState.cs
+++ src/Poe/RemoteMemoryObjects/IngameState.cs
@@ -14,14 +14,14 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
         public Element UIRoot => ReadObjectAt<Element>(0xC10 + Offsets.IgsOffset);
         public Element UIHover => ReadObjectAt<Element>(0xC14 + Offsets.IgsOffset);
         public int EntityLabelMap => M.ReadInt(Address + 0x50, 0x96C); // or 0x964, not sure
-        public DiagnosticInfoType DiagnosticInfoType => (DiagnosticInfoType)M.ReadInt(Address + 0xC8C);
-        public DiagnosticElement LatencyRectangle => GetObjectAt<DiagnosticElement>(0xE98);
-        public DiagnosticElement FrameTimeRectangle => GetObjectAt<DiagnosticElement>(0x12E8);
-        public DiagnosticElement FPSRectangle => GetObjectAt<DiagnosticElement>(0x1510);
-        public float CurLatency => M.ReadFloat(Address + 0xC90);
-        public float CurFrameTime => M.ReadFloat(Address + 0x10E0);
-        public float CurFps => M.ReadFloat(Address + 0x1308);
-        public TimeSpan TimeInGame => TimeSpan.FromMilliseconds(M.ReadInt(Address + 0xC70));
-        public int AreaChangeCount => M.ReadInt(Address + 0x19D0);
+        public DiagnosticInfoType DiagnosticInfoType => (DiagnosticInfoType)M.ReadInt(Address + 0xC8C + Offsets.IgsOffset);
+        public DiagnosticElement LatencyRectangle => GetObjectAt<DiagnosticElement>(0xE98 + Offsets.IgsOffset);
+        public DiagnosticElement FrameTimeRectangle => GetObjectAt<DiagnosticElement>(0x12E8 + Offsets.IgsOffset);
+        public DiagnosticElement FPSRectangle => GetObjectAt<DiagnosticElement>(0x1510 + Offsets.IgsOffset);
+        public float CurLatency => M.ReadFloat(Address + 0xC90 + Offsets.IgsOffset);
+        public float CurFrameTime => M.ReadFloat(Address + 0x10E0 + Offsets.IgsOffset);
+        public float CurFps => M.ReadFloat(Address + 0x1308 + Offsets.IgsOffset);
+        public TimeSpan TimeInGame => TimeSpan.FromMilliseconds(M.ReadInt(Address + 0xC70 + Offsets.IgsOffset));
+        public int AreaChangeCount => M.ReadInt(Address + 0x19D0 + Offsets.IgsOffset);
     }
 }
