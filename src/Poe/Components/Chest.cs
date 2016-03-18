89edaf06ef400cb6d1f5927feec5a4b872e9a932
TehCheat <thecheatuc@gmail.com>
2016-03-18 17:20:48 -0400

    Fixed chests.  Rebuilt.  Removed some unnecessary files.

--- src/Poe/Components/Chest.cs
+++ src/Poe/Components/Chest.cs
@@ -2,7 +2,7 @@ namespace PoeHUD.Poe.Components
 {
     public class Chest : Component
     {
-        public bool IsOpened => Address != 0 && M.ReadByte(Address + 0x24) == 1;
-        public bool IsStrongbox => Address != 0 && M.ReadInt(Address + 0x3C) != 0;
+        public bool IsOpened => Address != 0 && M.ReadByte(Address + 0x20) == 1;
+        public bool IsStrongbox => Address != 0 && M.ReadInt(Address + 0x34) != 0;
     }
 }
