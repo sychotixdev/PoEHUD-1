c922e00ee87ed452c874054500a7cfa43a5aa8bb
TehCheat <thecheatuc@gmail.com>
2016-06-30 07:47:44 -0400

    Updated preload_alerts and fixed areachangecount offset in preloads.

--- src/Hud/Preload/PreloadAlertPlugin.cs
+++ src/Hud/Preload/PreloadAlertPlugin.cs
@@ -121,7 +121,7 @@ namespace PoeHUD.Hud.Preload
                     // address is null, something has gone wrong, start over
                     return;
                 }
-                if (memory.ReadInt(listIterator + 0x8) == 0 || memory.ReadInt(listIterator + 0xC, 0x2C) != areaChangeCount) continue;
+                if (memory.ReadInt(listIterator + 0x8) == 0 || memory.ReadInt(listIterator + 0xC, 0x34) != areaChangeCount) continue;
                 string text = memory.ReadStringU(memory.ReadInt(listIterator + 8));
                 if (text.Contains('@')) { text = text.Split('@')[0]; }
                 CheckForPreload(text);
