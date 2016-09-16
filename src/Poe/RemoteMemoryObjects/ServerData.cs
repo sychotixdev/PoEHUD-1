6c770521240f333eb2b2b1e849ef5739f20483fb
TehCheat <thecheatuc@gmail.com>
2016-09-16 00:30:17 -0400

    Fixed the in game offset for real this time.  I think.

--- src/Poe/RemoteMemoryObjects/ServerData.cs
+++ src/Poe/RemoteMemoryObjects/ServerData.cs
@@ -2,7 +2,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class ServerData : RemoteMemoryObject
     {
-        public bool IsInGame => M.ReadInt(Address + /*Offsets.InGameOffset*/ 0x38EC) == 3;
+        public bool IsInGame => M.ReadInt(Address + /*Offsets.InGameOffset*/ 0x3934) == 3;
         public InventoryList PlayerInventories => GetObject<InventoryList>(Address + 0x2900);
     }
 }
