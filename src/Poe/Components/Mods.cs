cf342c9487243b9a3a99f6f9a865062cbf6f8869
TehCheat <thecheatuc@gmail.com>
2016-11-25 17:49:24 -0500

    Updated Mods Component Offsets.

--- src/Poe/Components/Mods.cs
+++ src/Poe/Components/Mods.cs
@@ -8,22 +8,21 @@ namespace PoeHUD.Poe.Components
 {
     public class Mods : Component
     {
-        public ItemRarity ItemRarity => Address != 0 ? (ItemRarity)M.ReadInt(Address + 0x58) : ItemRarity.Normal;
-        public int ItemLevel => Address != 0 ? M.ReadInt(Address + 0x118) : 1;
-        public int RequiredLevel => Address != 0 ? M.ReadInt(Address + 0x11C) : 1;
-        public string UniqueName => Address != 0 ? M.ReadStringU(M.ReadInt(Address + 0x18, 4, 4)) + M.ReadStringU(M.ReadInt(Address + 0x18, 0xC, 4)): string.Empty;
-        public bool Identified => Address != 0 && M.ReadByte(Address + 0x54) == 1;
-        public ItemStats ItemStats => new ItemStats(Owner);
-
+        public string UniqueName => Address != 0 ? M.ReadStringU(M.ReadInt(Address + 0x30, 0x8, 0x4)) + M.ReadStringU(M.ReadInt(Address + 0x30, 0x18, 4)) : string.Empty;
+        public bool Identified => Address != 0 && M.ReadByte(Address + 0x88) == 1;
+        public ItemRarity ItemRarity => Address != 0 ? (ItemRarity)M.ReadInt(Address + 0x8C) : ItemRarity.Normal;
         public List<ItemMod> ItemMods
         {
             get
             {
-                var implicitMods = GetMods(0x5C, 0x60);
-                var explicitMods = GetMods(0x68, 0x6C);
+                var implicitMods = GetMods(0x90, 0x98);
+                var explicitMods = GetMods(0xA8, 0xB0);
                 return implicitMods.Concat(explicitMods).ToList();
             }
         }
+        public int ItemLevel => Address != 0 ? M.ReadInt(Address + 0x198) : 1;
+        public int RequiredLevel => Address != 0 ? M.ReadInt(Address + 0x19C) : 1;
+        public ItemStats ItemStats => new ItemStats(Owner);
 
         private List<ItemMod> GetMods(int startOffset, int endOffset)
         {
@@ -33,11 +32,11 @@ namespace PoeHUD.Poe.Components
 
             int begin = M.ReadInt(Address + startOffset);
             int end = M.ReadInt(Address + endOffset);
-            int count = (end - begin) / 0x14;
+            int count = (end - begin) / 0x28;
             if (count > 12)
                 return list;
 
-            for (int i = begin; i < end; i += 0x14)
+            for (int i = begin; i < end; i += 0x28)
                 list.Add(GetObject<ItemMod>(i));
 
             return list;
