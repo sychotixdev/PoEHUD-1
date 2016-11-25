74e188bdd3da1b912ef27b0cdc235bd447f685ac
TehCheat <thecheatuc@gmail.com>
2016-11-25 17:48:14 -0500

    Fixed SkillID (it's a pointer) in Buff

--- src/Poe/RemoteMemoryObjects/Buff.cs
+++ src/Poe/RemoteMemoryObjects/Buff.cs
@@ -3,8 +3,8 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
     public class Buff : RemoteMemoryObject
     {
         public string Name => M.ReadStringU(M.ReadInt(Address + 8, 0));
-        public int Charges => M.ReadInt(Address + 0x28);
-        public int SkillId => M.ReadInt(Address + 0x34);
+        public byte Charges => M.ReadByte(Address + 0x28);
+        public int SkillId => M.ReadInt(Address + 0x50);
         public float Timer => M.ReadFloat(Address + 0x14);
     }
 }
