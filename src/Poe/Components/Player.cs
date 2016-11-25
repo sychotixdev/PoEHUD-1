fc3c36abc2cae841309ca402faa9ea5cdc3e139c
TehCheat <thecheatuc@gmail.com>
2016-11-25 17:48:50 -0500

    Updated Player Component Offsets.

--- src/Poe/Components/Player.cs
+++ src/Poe/Components/Player.cs
@@ -10,16 +10,16 @@ namespace PoeHUD.Poe.Components
                 {
                     return "";
                 }
-                int NameLength = M.ReadInt(Address + 0x20);
+                int NameLength = M.ReadInt(Address + 0x28);
                 if (NameLength > 512)
                 {
                     return "";
                 }
-                return NameLength < 8 ? M.ReadStringU(Address + 0x10, NameLength * 2) : M.ReadStringU(M.ReadInt(Address + 0x10), NameLength * 2);
+                return NameLength < 8 ? M.ReadStringU(Address + 0x20, NameLength * 2) : M.ReadStringU(M.ReadInt(Address + 0x20), NameLength * 2);
             }
         }
 
-        public long XP => Address != 0 ? M.ReadUInt(Address + 0x30) : 0L;
-        public int Level => Address != 0 ? M.ReadInt(Address + 0x40) : 1;
+        public long XP => Address != 0 ? M.ReadUInt(Address + 0x48) : 0L;
+        public int Level => Address != 0 ? M.ReadInt(Address + 0x58) : 1;
     }
 }
