fe787b9c37af5c911df82a39e0483be9b8c14bce
TehCheat <thecheatuc@gmail.com>
2017-03-03 14:31:57 -0500

    Updated UI Element offsets for 2.6.0.

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -1,27 +1,28 @@
 using PoeHUD.Poe.Elements;
 using System.Collections.Generic;
+using System.IO;
 
 namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class IngameUIElements : RemoteMemoryObject
     {
         public Element QuestTracker => ReadObjectAt<Element>(0xB58);
-        public Element OpenLeftPanel => ReadObjectAt<Element>(0xB98);
-        public Element OpenRightPanel => ReadObjectAt<Element>(0xBA0);
-        public Elements.Inventory InventoryPanel => ReadObjectAt<Elements.Inventory>(0xBD0);
-        public Element TreePanel => ReadObjectAt<Element>(0xC00);
-        public Element AtlasPanel => ReadObjectAt<Element>(0xC08);
-        public Map Map => ReadObjectAt<Map>(0xC38);
+        public Element OpenLeftPanel => ReadObjectAt<Element>(0xBA0);
+        public Element OpenRightPanel => ReadObjectAt<Element>(0xBA8);
+        public Element InventoryPanel => ReadObjectAt<Element>(0xBD8);
+        public Element TreePanel => ReadObjectAt<Element>(0xC08);
+        public Element AtlasPanel => ReadObjectAt<Element>(0xC10);
+        public Map Map => ReadObjectAt<Map>(0xC40);
         public IEnumerable<ItemsOnGroundLabelElement> ItemsOnGroundLabels
         {
             get
             {
-                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xC40);
+                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xC48);
                 return itemsOnGroundLabelRoot.Children;
             }
         }
-        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xDE8);
-        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xE08);
+        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xDF0);
+        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xE10);
     }
 }
 
