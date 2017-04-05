a71ed31c692a6e449928e47681082c6f3280a63e
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-04-05 03:18:16 -0400

    Added Player Stash Inventories support

---
+++ src/Poe/Elements/StashElement.cs
@@ -0,0 +1,52 @@
+﻿using PoeHUD.Poe.RemoteMemoryObjects;
+
+namespace PoeHUD.Poe.Elements
+{
+    public class StashElement : Element
+    {
+        private Element StashTitlePanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb28)) : null;
+        private Element StashInventoryPanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb38)) : null;
+        private Element StashListPanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb48)) : null;
+
+        public long TotalStashes => StashInventoryPanel.ChildCount;
+
+        public Inventory getStashInventory(int number)
+        {
+            if (number >= TotalStashes)
+                return null;
+            if (StashInventoryPanel.Children[number].ChildCount == 0)
+                return null;
+            return StashInventoryPanel.Children[number].Children[0].Children[0].AsObject<Inventory>();
+        }
+        public string getStashName(int number)
+        {
+            if (number >= TotalStashes)
+                return string.Empty;
+            long add = StashListPanel.Children[2].Children[number].Address;
+            string ret = M.ReadStringU(M.ReadLong(add + 0x690, 0x5D0) + 0x00);
+            if ( ret.Length == 0)
+                return M.ReadStringU(M.ReadLong(add + 0x690) + 0x5D0);
+            return ret;
+        }
+        public Element getStashTitleElement(string stashName)
+        {
+            long addr = 0;
+            for (int i = 0; i < StashTitlePanel.ChildCount; i++)
+            {
+                if (StashTitlePanel.Children[i].ChildCount == 0)
+                    continue;
+
+                addr = StashTitlePanel.Children[i].Children[0].Address;
+                if (addr == 0)
+                    continue;
+
+                string text = M.ReadStringU(M.ReadLong(addr + 0x390, 0x820));
+                if (text == string.Empty)
+                    text = M.ReadStringU(M.ReadLong(addr + 0x390) + 0x820);
+                if (text == stashName)
+                    return StashTitlePanel.Children[i];
+            }
+            return null;
+        }
+    }
+}
--- src/Poe/RemoteMemoryObjects/Inventory.cs
+++ src/Poe/RemoteMemoryObjects/Inventory.cs
@@ -1,3 +1,4 @@
+using PoeHUD.Poe.Elements;
 using System.Collections.Generic;
 
 namespace PoeHUD.Poe.RemoteMemoryObjects
@@ -5,16 +6,16 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
     public class Inventory : RemoteMemoryObject
     {
         public long ItemCount => M.ReadLong(Address + 0x410, 0x5F0, 0x50);
-        public int Width => 0;
-        public int Height => 0;
 
-        public List<Entity> Items
+        // Only works if inventory is opened and currently being viewed.
+        // Even if game have already fetched inventory data from server.
+        public List<InventoryItemIcon> InventoryItems
         {
             get
             {
-                var list = new List<Entity>();
-                // Max size of Player Inventory excluding stashes.
-                if (ItemCount > 60 || ItemCount <= 0)
+                var list = new List<InventoryItemIcon>();
+                // Max size of Quad Stash.
+                if (ItemCount > 576 || ItemCount <= 0)
                 {
                     return list;
                 }
@@ -23,13 +24,13 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
                 long itmAddr = 0;
 
                 long invIndex = 0;
-                long invAddr = M.ReadLong(Address + 0x410, 0x5F0, 0x30);
-                while ( itmIndex < ItemCount)
+                long invAddr = M.ReadLong(Address + 0x710);
+                while (itmIndex < ItemCount)
                 {
                     itmAddr = M.ReadLong(invAddr + (invIndex * 8));
-                    if ( itmAddr != 0 && !hashSet.Contains(itmAddr) )
+                    if (itmAddr != 0)
                     {
-                        list.Add(ReadObject<Entity>(itmAddr));
+                        list.Add(GetObject<InventoryItemIcon>(itmAddr));
                         hashSet.Add(itmAddr);
                         itmIndex++;
                     }
@@ -38,6 +39,9 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
                 return list;
             }
         }
+
+        // Works even if inventory is currently not in view.
+        // As long as game have fetched inventory data from Server.
         // Will return the item based on x,y format.
         // Give more controll to user what to do with
         // dublicate items (items taking more than 1 slot)
