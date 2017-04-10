2d7827524416513f778d6eea9f494517eb772e1d
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-04-10 10:38:09 -0400

    fixup Inventory Class bug.

--- src/Poe/Elements/InventoryElements/EssenceInventoryItem.cs
+++ src/Poe/Elements/InventoryElements/EssenceInventoryItem.cs
@@ -2,21 +2,12 @@ using SharpDX;
 
 namespace PoeHUD.Poe.Elements
 {
-    public class EssenceInventoryItem : Element
+    public class EssenceInventoryItem : NormalInventoryItem
     {
-        public int InventPosX => M.ReadInt(Address + 0xb20);
-        public int InventPosY => M.ReadInt(Address + 0xb24);
-
-        public Entity Item
-        {
-            get
-            {
-                return ReadObject<Entity>(Address + 0xB18);
-            }
-        }
-
-        public ToolTipType toolTipType => ToolTipType.InventoryItem;
-        public Element ToolTip => ReadObject<Element>(Address + 0xAD0);
+        // Inventory Position in Essence Stash is always invalid.
+        // Also, as items are fixed, so Inventory Position doesn't matter.
+        public override int InventPosX => 0;
+        public override int InventPosY => 0;
 
         public override RectangleF GetClientRect()
         {
