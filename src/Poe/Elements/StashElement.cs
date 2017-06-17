8c728183ee5ed0b326e53288c55c6673f184ca56
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-06-17 18:22:14 -0400

    fix up

--- src/Poe/Elements/StashElement.cs
+++ src/Poe/Elements/StashElement.cs
@@ -9,14 +9,15 @@ namespace PoeHUD.Poe.Elements
 
         private Element StashTitlePanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb28)) : null;
         private Element StashInventoryPanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb38)) : null;
-        private Element ViewAllStashPanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb48)) : null;
+        private Element StashListPanel => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA48, 0x10, 0xb48)) : null;
 
         public Element ExitButton => Address != 0 ? GetObject<Element>(M.ReadLong(Address + 0xA28)) : null;
         // TODO: Gonna convert this to Address + offset mode later on.
+        public Element ViewAllStashPanel => Address != 0 ? StashListPanel.Children[2] : null;
+        public Element SearchBar => Address != 0 ? this.Children[3].Children[3].Children[0] : null;
         public Element ViewAllStashButton => Address != 0 ? this.Children[2].Children[0].Children[1].Children[2] : null;
         public Element NextStashButton => Address != 0 ? this.Children[2].Children[0].Children[1].Children[5] : null;
         public Element PreviousStashButton => Address != 0 ? this.Children[2].Children[0].Children[1].Children[4] : null;
-        public Element SearchBar => Address != 0 ? this.Children[3].Children[3].Children[0] : null;
         public Element ClearSearchButton => Address != 0 ? this.Children[3].Children[3].Children[1] : null;
 
         public Inventory getStashInventory(int number)
@@ -31,7 +32,7 @@ namespace PoeHUD.Poe.Elements
         {
             if (number >= TotalStashes || number < 0)
                 return string.Empty;
-            long add = ViewAllStashPanel.Children[2].Children[number].Address;
+            long add = StashListPanel.Children[2].Children[number].Address;
             int NameLength = M.ReadInt(add + 0x690, 0x5E0);
             return NameLength < 8 ? M.ReadStringU(M.ReadLong(add + 0x690) + 0x5D0, NameLength * 2) : M.ReadStringU(M.ReadLong(add + 0x690, 0x5D0), NameLength * 2);
         }
