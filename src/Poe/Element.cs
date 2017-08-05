bf2f7e4698d9ecb795eae515c3464eda4c6617d5
TehCheat <thecheatuc@gmail.com>
2017-08-04 21:46:58 -0400

    Updated element root and parent, which was breaking some UI interactions.  Rebuilt exe.

--- src/Poe/Element.cs
+++ src/Poe/Element.cs
@@ -15,8 +15,8 @@ namespace PoeHUD.Poe
         public int vTable => M.ReadInt(Address + 0);
         public long ChildCount => (M.ReadLong(Address + 0x44 + OffsetBuffers) - M.ReadLong(Address + 0x3c + OffsetBuffers)) / 8;
         public bool IsVisibleLocal => (M.ReadInt(Address + 0x94 + OffsetBuffers) & 1) == 1;
-        public Element Root => ReadObject<Element>(Address + 0x9C + OffsetBuffers);
-        public Element Parent => ReadObject<Element>(Address + 0xA4 + OffsetBuffers);
+        public Element Root => ReadObject<Element>(Address + 0xC4 + OffsetBuffers);
+        public Element Parent => ReadObject<Element>(Address + 0xCC + OffsetBuffers);
         public float X => M.ReadFloat(Address + 0xD4 + OffsetBuffers);
         public float Y => M.ReadFloat(Address + 0xD8 + OffsetBuffers);
         public float Scale => M.ReadFloat(Address + 0x1D0 + OffsetBuffers);
