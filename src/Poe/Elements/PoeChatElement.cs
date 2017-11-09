fe6fa502906dda9cee17777fa0d6b27b39d5a4b0
Stridemann <stridemann@gmail.com>
2017-11-09 09:46:12 +0300

    Improvements chat messages

--- src/Poe/Elements/PoeChatElement.cs
+++ src/Poe/Elements/PoeChatElement.cs
@@ -1,18 +1,29 @@
-﻿namespace PoeHUD.Poe.Elements
+﻿using System.Collections.Generic;
+
+namespace PoeHUD.Poe.Elements
 {
     public class PoeChatElement : Element
     {
         //private Element ChatBoxParent => GetObject<Element>(M.ReadLong(Address + 0xC78, 0x408));
         public Element ChatBoxParent => GetObject<Element>(M.ReadLong(Address + 0xAB0, 0xC28));
-        public long totalChatMessages => ChatBoxParent.ChildCount;
+        public long TotalChatMessages => ChatBoxParent.ChildCount;
         public string this[int number]
         {
             get
             {
-                int string_length = M.ReadInt(ChatBoxParent.Children[number].Address + 0xC28);
-                int unknown = +M.ReadInt(ChatBoxParent.Children[number].Address + 0xC30);
-                return M.ReadStringU(M.ReadLong(ChatBoxParent.Children[number].Address + 0xC18), string_length * 2);
+                var parentChildrenAddr = ChatBoxParent.Children[number].Address;
+                int length = M.ReadInt(parentChildrenAddr + 0xC28);
+                //int unknown = M.ReadInt(parentChildrenAddr + 0xC30);
+                return M.ReadStringU(M.ReadLong(parentChildrenAddr + 0xC18), length * 2);
             }
+        }
+
+        public List<string> GetChatMessages()
+        {
+            List<string> rezult = new List<string>();
+            for (int i = 0; i < TotalChatMessages; ++i)
+                rezult.Add(this[i]);
+            return rezult;
         }
     }
 }
