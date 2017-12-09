0387fb29729ef88ecc0f5f0bc8ad750f30d25b2e
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-12-09 09:32:57 -0500

    added buff class max timer. recompiled exe

--- src/Poe/RemoteMemoryObjects/Buff.cs
+++ src/Poe/RemoteMemoryObjects/Buff.cs
@@ -5,6 +5,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
         public string Name => M.ReadStringU(M.ReadLong(Address + 8, 0));
         public byte Charges => M.ReadByte(Address + 0x2C);
         //public int SkillId => M.ReadInt(Address + 0x5C); // I think this is part of another structure referenced in a pointer at 0x58
-        public float Timer => M.ReadFloat(Address + 0x14);
+        public float MaxTime => M.ReadFloat(Address + 0x10); // infinity for auras and always on buff
+        public float Timer => M.ReadFloat(Address + 0x14); // timeleft
     }
 }
