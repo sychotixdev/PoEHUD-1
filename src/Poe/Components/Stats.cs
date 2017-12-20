87d3375e2474b3e14da6dfd0a27e3355674fc725
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-12-19 22:31:15 -0500

    fixup Stats class, which changes when inTown and when not in Town

--- src/Poe/Components/Stats.cs
+++ src/Poe/Components/Stats.cs
@@ -2,9 +2,11 @@
 {
     public class Stats : Component
     {
-        public int coldResistance => M.ReadInt(Address + 0x50, 0x154);
-        public int fireResistance => M.ReadInt(Address + 0x50, 0x15C);
-        public int lightningResistance => M.ReadInt(Address + 0x50, 0x164);
-        public int chaosResistance => M.ReadInt(Address + 0x50, 0x16C);
+        private int statOffset => (Game.IngameState.Data.CurrentArea.IsTown ||
+            Game.IngameState.Data.CurrentArea.Name.Contains("Hideout")) ? 0x00 : 0x08;
+        public int coldResistance => M.ReadInt(Address + 0x50, 0x154 + statOffset);
+        public int fireResistance => M.ReadInt(Address + 0x50, 0x15C + statOffset);
+        public int lightningResistance => M.ReadInt(Address + 0x50, 0x164 + statOffset);
+        public int chaosResistance => M.ReadInt(Address + 0x50, 0x16C + statOffset);
     }
 }
