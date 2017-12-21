c2637a7783f7c5ec86c6f022bd026c6016e705e0
Zaafar Ahmed <zaafar.tahir@gmail.com>
2017-12-20 19:53:01 -0500

    added Stats Class in PoeHUD containing all player stats

---
+++ src/Models/Enums/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿namespace PoeHUD.Models.Enums
+{
+    public enum PlayerStats
+    {
+        ColdResistance = 308,
+        FireResistance = 309,
+        LightningResistance = 310,
+        ChaosResistance = 311
+    }
+}
--- src/Poe/Components/Stats.cs
+++ src/Poe/Components/Stats.cs
@@ -1,7 +1,23 @@
-﻿namespace PoeHUD.Poe.Components
+﻿using PoeHUD.Models.Enums;
+
+namespace PoeHUD.Poe.Components
 {
     public class Stats : Component
     {
-
+        public bool getStat(PlayerStats key, out int value)
+        {
+            long ptrStart = M.ReadLong(Address + 0x50);
+            long ptrEnd = M.ReadLong(Address + 0x58);
+            for (long i = ptrStart; i < ptrEnd; i+=8)
+            {
+                if (M.ReadInt(i) == (int)key)
+                {
+                    value = M.ReadInt(i + 0x04);
+                    return true;
+                }
+            }
+            value = 0;
+            return false;
+        }
     }
 }
