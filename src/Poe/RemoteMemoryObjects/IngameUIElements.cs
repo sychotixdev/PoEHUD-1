1712a87c6bc986967804c3cd95c054fce30a9ce1
TehCheat <thecheatuc@gmail.com>
2018-01-26 17:52:34 -0500

    Reverted Mods offsets. Shifted UI elements for Garena UI element. Rebuilt exe.

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -5,26 +5,27 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class IngameUIElements : RemoteMemoryObject
     {
-        public SkillBarElement SkillBar => ReadObjectAt<SkillBarElement>(0xB50);
-        public SkillBarElement HiddenSkillBar => ReadObjectAt<SkillBarElement>(0xB58);
-        public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.ReadLong(Address + 0xBD0, 0xAB8, 0xC30));
-        public Element QuestTracker => ReadObjectAt<Element>(0xC00);
-        public Element OpenLeftPanel => ReadObjectAt<Element>(0xC40);
-        public Element OpenRightPanel => ReadObjectAt<Element>(0xC48);
-        public InventoryElement InventoryPanel => ReadObjectAt<InventoryElement>(0xC78);
-        public Element TreePanel => ReadObjectAt<Element>(0xCA8);
-        public Element AtlasPanel => ReadObjectAt<Element>(0xCB0);
-        public Map Map => ReadObjectAt<Map>(0xD00);
+
+        public SkillBarElement SkillBar => ReadObjectAt<SkillBarElement>(0xB58);
+        public SkillBarElement HiddenSkillBar => ReadObjectAt<SkillBarElement>(0xB60);
+        public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.ReadLong(Address + 0xBD8, 0xAB8, 0xC30));
+        public Element QuestTracker => ReadObjectAt<Element>(0xC08);
+        public Element OpenLeftPanel => ReadObjectAt<Element>(0xC48);
+        public Element OpenRightPanel => ReadObjectAt<Element>(0xC50);
+        public InventoryElement InventoryPanel => ReadObjectAt<InventoryElement>(0xC80);
+        public Element TreePanel => ReadObjectAt<Element>(0xCB0);
+        public Element AtlasPanel => ReadObjectAt<Element>(0xCB8);
+        public Map Map => ReadObjectAt<Map>(0xD08);
         public IEnumerable<ItemsOnGroundLabelElement> ItemsOnGroundLabels
         {
             get
             {
-                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xD08);
+                var itemsOnGroundLabelRoot = ReadObjectAt<ItemsOnGroundLabelElement>(0xD10);
                 return itemsOnGroundLabelRoot.Children;
             }
         }
-        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xF18);
-        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xF80);
+        public Element GemLvlUpPanel => ReadObjectAt<Element>(0xF20);
+        public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xF88);
     }
 }
 
