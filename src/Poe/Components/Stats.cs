a7ee232016835bdc7dd0b90803e06ede9b9e2642
Stridemann <stridemann@gmail.com>
2018-02-19 20:11:26 +0200

    Added GetStats to Stats component

--- src/Poe/Components/Stats.cs
+++ src/Poe/Components/Stats.cs
@@ -1,8 +1,25 @@
-﻿namespace PoeHUD.Poe.Components
+﻿using System.Collections.Generic;
+using PoeHUD.Models.Enums;
+
+namespace PoeHUD.Poe.Components
 {
     public class Stats : Component
     {
         public long statPtrStart => M.ReadLong(Address + 0x50);
         public long statPtrEnd => M.ReadLong(Address + 0x58);
+
+        //Stats goes as sequence of 2 values, 4 byte each. First goes stat ID then goes stat value
+        public Dictionary<PlayerStats, int> GetStats()
+        {
+            var result = new Dictionary<PlayerStats, int>();
+            for(var addr = statPtrStart; addr < statPtrEnd; addr += 8)
+            {
+                var statId = M.ReadInt(addr);
+                var statValue = M.ReadInt(addr + 4);
+
+                result.Add((PlayerStats)statId, statValue);
+            }
+            return result;
+        }
     }
 }
