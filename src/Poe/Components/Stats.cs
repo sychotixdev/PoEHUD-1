100e9757810991923ff0106b5c5bfd0206ab81e9
Zaafar Ahmed <zaafar.tahir@gmail.com>
2018-02-19 17:56:44 -0500

    optimizaed getstats function

--- src/Poe/Components/Stats.cs
+++ src/Poe/Components/Stats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using PoeHUD.Models.Enums;
 
 namespace PoeHUD.Poe.Components
@@ -11,13 +12,16 @@ namespace PoeHUD.Poe.Components
         //Stats goes as sequence of 2 values, 4 byte each. First goes stat ID then goes stat value
         public Dictionary<PlayerStats, int> GetStats()
         {
-            var result = new Dictionary<PlayerStats, int>();
-            for(var addr = statPtrStart; addr < statPtrEnd; addr += 8)
+            int key = 0;
+            int value = 0;
+            int total_stats = (int)(statPtrEnd - statPtrStart);
+            var bytes = M.ReadBytes(statPtrStart, total_stats);
+            var result = new Dictionary<PlayerStats, int>(total_stats/8);
+            for (int i = 0; i < bytes.Length; i += 8)
             {
-                var statId = M.ReadInt(addr);
-                var statValue = M.ReadInt(addr + 4);
-
-                result.Add((PlayerStats)statId, statValue);
+                key = BitConverter.ToInt32(bytes, i);
+                value = BitConverter.ToInt32(bytes, i + 0x04);
+                result[(PlayerStats)key] = value;
             }
             return result;
         }
