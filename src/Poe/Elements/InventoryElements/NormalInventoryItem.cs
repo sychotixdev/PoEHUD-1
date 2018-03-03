2e5a88cd73c888eb0b980d2ca273abd0162e2653
Stridemann <stridemann@gmail.com>
2018-03-03 14:14:59 +0200

    Lags is gone. InventoryItem fixed

--- src/Poe/Elements/InventoryElements/NormalInventoryItem.cs
+++ src/Poe/Elements/InventoryElements/NormalInventoryItem.cs
@@ -2,17 +2,9 @@ namespace PoeHUD.Poe.Elements
 {
     public class NormalInventoryItem : Element
     {
-        public virtual int InventPosX => M.ReadInt(Address + 0xb68);
-        public virtual int InventPosY => M.ReadInt(Address + 0xb6C);
-
-        public Entity Item
-        {
-            get
-            {
-                return ReadObject<Entity>(Address + 0xB60);
-            }
-        }
-
+        public virtual int InventPosX => M.ReadInt(Address + 0xb70);
+        public virtual int InventPosY => M.ReadInt(Address + 0xb74);
+        public Entity Item => ReadObject<Entity>(Address + 0xB68);
         public ToolTipType toolTipType => ToolTipType.InventoryItem;
         public Element ToolTip => ReadObject<Element>(Address + 0xB10);
 
