7103e84f5c2571b943989588a9978d1c65cb177f
Zaafar Ahmed <zaafar.tahir@gmail.com>
2018-03-05 20:23:50 -0500

    fixed imgui mouse hook

--- src/Poe/RemoteMemoryObjects/IngameState.cs
+++ src/Poe/RemoteMemoryObjects/IngameState.cs
@@ -21,28 +21,35 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
         private IngameData DataReal => ReadObject<IngameData>(Address + 0x1F0 + Offsets.IgsOffset);
 
         public bool InGame => ServerDataReal.IsInGame;
+
         public ServerData ServerData =>_cache.Enable && _cache.ServerData!=null ?_cache.ServerData : _cache.Enable? _cache.ServerData=ServerDataReal : ServerDataReal;
-
         private ServerData ServerDataReal => ReadObjectAt<ServerData>(0x1F8 + Offsets.IgsOffset);
+
         public IngameUIElements IngameUi =>_cache.Enable && _cache.IngameUi!=null ?_cache.IngameUi : _cache.Enable? _cache.IngameUi=IngameUiReal : IngameUiReal;
-
         private IngameUIElements IngameUiReal => ReadObjectAt<IngameUIElements>(0x650 + Offsets.IgsOffset);
-        public Element UIRoot =>_cache.Enable && _cache.UIRoot!=null ?_cache.UIRoot : _cache.Enable ? _cache.UIRoot=UIRootReal: UIRootReal;
 
+        public Element UIRoot =>_cache.Enable && _cache.UIRoot!=null ?_cache.UIRoot : _cache.Enable ? _cache.UIRoot=UIRootReal: UIRootReal;
         private Element UIRootReal => ReadObjectAt<Element>(0xD00 + Offsets.IgsOffset);
+
         public Element UIHover => ReadObjectAt<Element>(0xD38 + Offsets.IgsOffset);
+        public float UIHoverX => M.ReadFloat(Address + 0xD40 + Offsets.IgsOffset);
+        public float UIHoverY => M.ReadFloat(Address + 0xD44 + Offsets.IgsOffset);
+        public Element UIHoverTooltip => ReadObjectAt<Element>(0xD48 + Offsets.IgsOffset);
 
         public float CurentUIElementPosX => M.ReadFloat(Address + 0x940 + Offsets.IgsOffset);
         public float CurentUIElementPosY => M.ReadFloat(Address + 0x944 + Offsets.IgsOffset);
 
         public long EntityLabelMap => M.ReadLong(Address + 0x98, 0xA78);
         public DiagnosticInfoType DiagnosticInfoType => (DiagnosticInfoType)M.ReadInt(Address + 0xDE0 + Offsets.IgsOffset);
-        public DiagnosticElement LatencyRectangle =>_cache.Enable && _cache.LatencyRectangle!=null ?_cache.LatencyRectangle : _cache.Enable ? _cache.LatencyRectangle=LatencyRectangleReal: LatencyRectangleReal;
 
+        public DiagnosticElement LatencyRectangle =>_cache.Enable && _cache.LatencyRectangle!=null ?_cache.LatencyRectangle : _cache.Enable ? _cache.LatencyRectangle=LatencyRectangleReal: LatencyRectangleReal;
         private DiagnosticElement LatencyRectangleReal => GetObjectAt<DiagnosticElement>(0x1010 + Offsets.IgsOffset);
+
         public DiagnosticElement FrameTimeRectangle => GetObjectAt<DiagnosticElement>(0x14A0 + Offsets.IgsOffset);
+
         public DiagnosticElement FPSRectangle =>_cache.Enable && _cache.FPSRectangle!=null ?_cache.FPSRectangle : _cache.Enable? _cache.FPSRectangle=FPSRectangleReal: FPSRectangleReal;
         private DiagnosticElement FPSRectangleReal => GetObjectAt<DiagnosticElement>(0x16E8 + Offsets.IgsOffset);
+
         public float CurLatency => LatencyRectangle.CurrValue;
         public float CurFrameTime => FrameTimeRectangle.CurrValue;
         public float CurFps => FPSRectangle.CurrValue;
