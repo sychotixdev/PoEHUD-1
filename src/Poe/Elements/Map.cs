66db749087fb6987ef30f2ff913cedc3b0dc3305
TehCheat <thecheatuc@gmail.com>
2018-03-21 15:27:08 -0400

    Fixed OrangeWords offset.

--- src/Poe/Elements/Map.cs
+++ src/Poe/Elements/Map.cs
@@ -7,7 +7,7 @@ namespace PoeHUD.Poe.Elements
         //public Element MapProperties => ReadObjectAt<Element>(0x1FC + OffsetBuffers);
         public Element LargeMap => ReadObjectAt<Element>(0x274 + OffsetBuffers);
         public Element SmallMinimap => ReadObjectAt<Element>(0x278 + OffsetBuffers);
-        public Element OrangeWords => ReadObjectAt<Element>(0x28 + OffsetBuffers);
+        public Element OrangeWords => ReadObjectAt<Element>(0x284 + OffsetBuffers);
         public Element BlueWords => ReadObjectAt<Element>(0x2A0 + OffsetBuffers);
     }
 }
