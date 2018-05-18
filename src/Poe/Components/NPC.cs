cf84eb92706b8bc371c181e60e2044041bc53b50
Stridemann <stridemann@gmail.com>
2018-05-18 22:56:39 +0300

    Added useless offsets to NPC component

--- src/Poe/Components/NPC.cs
+++ src/Poe/Components/NPC.cs
@@ -2,5 +2,8 @@
 {
     public class NPC : Component
     {
+        public bool HasIconOverhead => M.ReadLong(Address + 0x48) != 0;
+        public bool IsIgnoreHidden => M.ReadByte(Address + 0x20) == 1;
+        public bool IsMinimapLabelVisible => M.ReadByte(Address + 0x21) == 1;
     }
 }
