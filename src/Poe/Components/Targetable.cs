87c279b729cb928bd2c6d22326a50cff30dae000
TehCheat <TehCheat@users.noreply.github.com>
2018-06-11 12:03:29 -0400

    Updated/added Targetable component offsets.

--- src/Poe/Components/Targetable.cs
+++ src/Poe/Components/Targetable.cs
@@ -2,6 +2,7 @@ namespace PoeHUD.Poe.Components
 {
     public class Targetable : Component
     {
-        public bool isTargeted => Address != 0 && M.ReadByte(Address + 0x2A) == 1;
+        public bool isTargetable => Address != 0 && M.ReadByte(Address + 0x30) == 1;
+        public bool isTargeted => Address != 0 && M.ReadByte(Address + 0x32) == 1;
     }
 }
