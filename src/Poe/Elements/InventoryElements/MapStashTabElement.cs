8d6617d3689e31ae57b4581393fbc8cb42b2e479
Zaafar Ahmed <zaafar.tahir@gmail.com>
2018-06-30 16:51:50 -0400

    initial support for mapstashtab map count.

---
+++ src/Poe/Elements/InventoryElements/MapStashTabElement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace PoeHUD.Poe.Elements
+{
+
+    public class MapStashTabElement : Element
+    {
+        private long mapListStartPtr => (Address != 0) ? M.ReadLong(Address + 0x9D8) : 0x00;
+        private long mapListEndPtr => (Address != 0) ? M.ReadLong(Address + 0x9D8 + 0x08) : 0x00;
+
+        public int TotalInventories => (int)((mapListEndPtr - mapListStartPtr) / 0x10);
+
+        public int SubInventoryMapTier(int index)
+        {
+            return M.ReadInt(mapListStartPtr + (index * 0x10), 0x00);
+        }
+
+        public int SubInventoryMapId(int index)
+        {
+            return M.ReadInt(mapListStartPtr + (index * 0x10), 0x04);
+        }
+
+        public int SubInventoryMapCount(int index)
+        {
+            return M.ReadInt(mapListStartPtr + (index * 0x10), 0x08);
+        }
+    }
+}
