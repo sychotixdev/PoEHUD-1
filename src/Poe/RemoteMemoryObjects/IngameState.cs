f01bb9406c653331e184da4ef8a36b956fc8f177
TehCheat <thecheatuc@gmail.com>
2019-03-09 17:58:48 -0500

    Fixed some InGameState offsets.

--- src/Poe/RemoteMemoryObjects/IngameState.cs
+++ src/Poe/RemoteMemoryObjects/IngameState.cs
@@ -25,7 +25,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		public float CurentUIElementPosY => M.ReadFloat(Address + 0x528 + Offsets.IgsOffset);
 
 		public Element UIRoot => _cache.Enable && _cache.UIRoot != null ? _cache.UIRoot : _cache.Enable ? _cache.UIRoot = UIRootReal : UIRootReal;
-		private Element UIRootReal => ReadObjectAt<Element>(0xE90 + Offsets.IgsOffset);
+		private Element UIRootReal => ReadObjectAt<Element>(0x4A0 + Offsets.IgsOffset);
 		public Element UIHoverTooltip => ReadObjectAt<Element>(0x4E8 + Offsets.IgsOffset);
 		public float UIHoverX => M.ReadFloat(Address + 0x51C + Offsets.IgsOffset);
 		public float UIHoverY => M.ReadFloat(Address + 0x520 + Offsets.IgsOffset);
@@ -34,15 +34,15 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		public TimeSpan TimeInGame => TimeSpan.FromSeconds(M.ReadFloat(Address + 0x570 + Offsets.IgsOffset));
 		public float TimeInGameF => M.ReadFloat(Address + 0x570 + Offsets.IgsOffset);
 
-		public DiagnosticInfoType DiagnosticInfoType => (DiagnosticInfoType)M.ReadInt(Address + 0xF68 + Offsets.IgsOffset);
+		public DiagnosticInfoType DiagnosticInfoType => (DiagnosticInfoType)M.ReadInt(Address + 0x588 + Offsets.IgsOffset);
 
 		public DiagnosticElement LatencyRectangle => _cache.Enable && _cache.LatencyRectangle != null ? _cache.LatencyRectangle : _cache.Enable ? _cache.LatencyRectangle = LatencyRectangleReal : LatencyRectangleReal;
-		private DiagnosticElement LatencyRectangleReal => GetObjectAt<DiagnosticElement>(0x1198 + Offsets.IgsOffset);
+		private DiagnosticElement LatencyRectangleReal => GetObjectAt<DiagnosticElement>(0x7BB + Offsets.IgsOffset);
 
-		public DiagnosticElement FrameTimeRectangle => GetObjectAt<DiagnosticElement>(0x1628 + Offsets.IgsOffset);
+		public DiagnosticElement FrameTimeRectangle => GetObjectAt<DiagnosticElement>(0xC48 + Offsets.IgsOffset);
 
 		public DiagnosticElement FPSRectangle => _cache.Enable && _cache.FPSRectangle != null ? _cache.FPSRectangle : _cache.Enable ? _cache.FPSRectangle = FPSRectangleReal : FPSRectangleReal;
-		private DiagnosticElement FPSRectangleReal => GetObjectAt<DiagnosticElement>(0x1870 + Offsets.IgsOffset);
+		private DiagnosticElement FPSRectangleReal => GetObjectAt<DiagnosticElement>(0xE90 + Offsets.IgsOffset);
 
 		public float CurLatency => LatencyRectangle.CurrValue;
 		public float CurFrameTime => FrameTimeRectangle.CurrValue;
@@ -51,6 +51,6 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		public Camera Camera => _cache.Enable && _cache.Camera != null ? _cache.Camera :
 			_cache.Enable ? _cache.Camera = CameraReal : CameraReal;
 
-		private Camera CameraReal => GetObject<Camera>(Address + 0x1258 + Offsets.IgsOffsetDelta);
+		private Camera CameraReal => GetObject<Camera>(Address + 0xF54 + Offsets.IgsOffsetDelta);
 	}
 }
