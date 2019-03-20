1fe42adb49e15b27f0b24fb709ba00dfd8e5a823
TehCheat <thecheatuc@gmail.com>
2019-03-19 22:14:00 -0400

    Fixed ItemOnGroundLabelElement offset. Tooltips for items on ground should work now.

--- src/Poe/Elements/HoverItemIcon.cs
+++ src/Poe/Elements/HoverItemIcon.cs
@@ -60,7 +60,7 @@ namespace PoeHUD.Poe.Elements
                 {
                     case ToolTipType.ItemOnGround:
                         // This offset is same as Game.IngameState.IngameUi.ItemsOnGroundLabels offset.
-                        ItemsOnGroundLabelElement le = Game.IngameState.IngameUi.ReadObjectAt<ItemsOnGroundLabelElement>(0xD58);
+                        ItemsOnGroundLabelElement le = Game.IngameState.IngameUi.ReadObjectAt<ItemsOnGroundLabelElement>(0x5A8);
                         Entity e = le.ItemOnHover;
                         if (e == null)
                             return null;
