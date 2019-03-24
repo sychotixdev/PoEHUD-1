05417c662ea4d4fcd79d6fdc33a5a530903be91a
Zaafar Ahmed <zaafar.tahir@gmail.com>
2019-03-24 00:45:30 -0400

    Update Element.cs

--- src/Poe/Element.cs
+++ src/Poe/Element.cs
@@ -22,7 +22,7 @@ namespace PoeHUD.Poe
         public Element Parent => ReadObject<Element>(Address + 0x90 + OffsetBuffers);
         public float X => M.ReadFloat(Address + 0x98 + OffsetBuffers);
         public float Y => M.ReadFloat(Address + 0x9c + OffsetBuffers);
-        public Element Tooltip => ReadObject<Element>(Address + 0x340); //0x7F0
+        public Element Tooltip => ReadObject<Element>(Address + 0x338); //0x7F0
         public float Scale => M.ReadFloat(Address + 0x108 + OffsetBuffers);
         public float Width => M.ReadFloat(Address + 0x130 + OffsetBuffers);
         public float Height => M.ReadFloat(Address + 0x134 + OffsetBuffers);
