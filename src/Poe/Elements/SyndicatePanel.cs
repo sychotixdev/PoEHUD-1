a6b5f76a80b5d3891224429c721625613fa34353
Stridemann <stridemann@gmail.com>
2019-05-06 22:43:37 +0300

    Fixed SyndicatePanel pos

--- src/Poe/Elements/SyndicatePanel.cs
+++ src/Poe/Elements/SyndicatePanel.cs
@@ -1,8 +1,8 @@
 ﻿namespace PoeHUD.Poe.Elements
 {
 	public class SyndicatePanel : Element
-	{
-		public Element EventElement => ReadObjectAt<Element>(0xAD8);
+    {
+        public Element EventElement => GetChildFromIndices(0, 24);
 		public Element TextElement => EventElement.GetChildFromIndices(5, 1);
 		public string EventText => TextElement.AsObject<EntityLabel>().Text;
 	}
