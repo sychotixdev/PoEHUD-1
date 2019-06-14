38aabe6c5c9a09eae46b35378e00305c847f086c
TehCheat <thecheatuc@gmail.com>
2019-06-13 23:20:58 -0400

    Updated offsets for 3.7.1

--- src/Poe/Components/Life.cs
+++ src/Poe/Components/Life.cs
@@ -6,16 +6,16 @@ namespace PoeHUD.Poe.Components
 {
 	public class Life : Component
 	{
-		public int MaxHP => Address != 0 ? M.ReadInt(Address + 0x88) : 1;
-		public int CurHP => Address != 0 ? M.ReadInt(Address + 0x8C) : 1;
-		public int ReservedFlatHP => Address != 0 ? M.ReadInt(Address + 0x94) : 0;
-		public int ReservedPercentHP => Address != 0 ? M.ReadInt(Address + 0x98) : 0;
-		public int MaxMana => Address != 0 ? M.ReadInt(Address + 0xD8) : 1;
-		public int CurMana => Address != 0 ? M.ReadInt(Address + 0xDC) : 1;
-		public int ReservedFlatMana => Address != 0 ? M.ReadInt(Address + 0xE4) : 0;
-		public int ReservedPercentMana => Address != 0 ? M.ReadInt(Address + 0xE8) : 0;
-		public int MaxES => Address != 0 ? M.ReadInt(Address + 0x110) : 0;
-		public int CurES => Address != 0 ? M.ReadInt(Address + 0x114) : 0;
+		public int MaxHP => Address != 0 ? M.ReadInt(Address + 0xA0) : 1;
+		public int CurHP => Address != 0 ? M.ReadInt(Address + 0xA4) : 1;
+		public int ReservedFlatHP => Address != 0 ? M.ReadInt(Address + 0xAC) : 0;
+		public int ReservedPercentHP => Address != 0 ? M.ReadInt(Address + 0xB0) : 0;
+		public int MaxMana => Address != 0 ? M.ReadInt(Address + 0xF0) : 1;
+		public int CurMana => Address != 0 ? M.ReadInt(Address + 0xF4) : 1;
+		public int ReservedFlatMana => Address != 0 ? M.ReadInt(Address + 0xFC): 0;
+		public int ReservedPercentMana => Address != 0 ? M.ReadInt(Address + 0x100) : 0;
+		public int MaxES => Address != 0 ? M.ReadInt(Address + 0x128) : 0;
+		public int CurES => Address != 0 ? M.ReadInt(Address + 0x12C) : 0;
 		public float HPPercentage => CurHP / (float)(MaxHP - ReservedFlatHP - Math.Round(ReservedPercentHP * 0.01 * MaxHP));
 		public float MPPercentage => CurMana / (float)(MaxMana - ReservedFlatMana - Math.Round(ReservedPercentMana * 0.01 * MaxMana));
 
@@ -32,8 +32,8 @@ namespace PoeHUD.Poe.Components
 		}
 
 		//public bool CorpseUsable => M.ReadBytes(Address + 0x238, 1)[0] == 1; // Total guess, didn't verify
-		private long BuffStart => M.ReadLong(Address + 0x140);
-		private long BuffEnd => M.ReadLong(Address + 0x148);
+		private long BuffStart => M.ReadLong(Address + 0x158);
+		private long BuffEnd => M.ReadLong(Address + 0x160);
 		private long MaxBuffCount => 512; // Randomly bumping to 512 from 32 buffs... no idea what real value is.
 		public List<Buff> Buffs
 		{
--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -8,33 +8,33 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 {
 	public class IngameUIElements : RemoteMemoryObject
 	{
-		public SkillBarElement SkillBar => ReadObjectAt<SkillBarElement>(0x370);
-		public SkillBarElement HiddenSkillBar => ReadObjectAt<SkillBarElement>(0x378);
-		public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.ReadLong(Address + 0x3F8, 0x2D0, 0xF80));
-		public Element QuestTracker => ReadObjectAt<Element>(0x478);
-		public Element OpenLeftPanel => ReadObjectAt<Element>(0x4E0/*4F0*/);
-		public Element OpenRightPanel => ReadObjectAt<Element>(0x4E8/*4F8 */);
-		public InventoryElement InventoryPanel => ReadObjectAt<InventoryElement>(0x518);
-		public StashElement StashElement => ReadObjectAt<StashElement>(0x520); //This element was in serverdata
-		public Element TreePanel => ReadObjectAt<Element>(0x548);
-		public Element AtlasPanel => ReadObjectAt<Element>(0x550);
-		public Map Map => ReadObjectAt<Map>(0x5A0);
+		public SkillBarElement SkillBar => ReadObjectAt<SkillBarElement>(0x388);
+		public SkillBarElement HiddenSkillBar => ReadObjectAt<SkillBarElement>(0x390);
+		public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.ReadLong(Address + 0x410, 0x2D0, 0xF80));
+		public Element QuestTracker => ReadObjectAt<Element>(0x490);
+		public Element OpenLeftPanel => ReadObjectAt<Element>(0x4F8/*508*/);
+		public Element OpenRightPanel => ReadObjectAt<Element>(0x500/*510 */);
+		public InventoryElement InventoryPanel => ReadObjectAt<InventoryElement>(0x530);
+		public StashElement StashElement => ReadObjectAt<StashElement>(0x538); //This element was in serverdata
+		public Element TreePanel => ReadObjectAt<Element>(0x560);
+		public Element AtlasPanel => ReadObjectAt<Element>(0x568);
+		public Map Map => ReadObjectAt<Map>(0x5B8);
         public SyndicatePanel SyndicatePanel => GameController.Instance.Game.IngameState.UIRoot.GetChildFromIndices(1, 64).AsObject<SyndicatePanel>();
-        public SubterraneanChart MineMap => ReadObjectAt<SubterraneanChart>(0x6F8/*0xED8*/);
-		public WorldMapElement WorldMap => ReadObjectAt<WorldMapElement>(0xCC0);
-		public WorldMapElement AreaInstanceUi => ReadObjectAt<WorldMapElement>(0x7A8);
+        public SubterraneanChart MineMap => ReadObjectAt<SubterraneanChart>(0x710/*0xED8*/);
+		public WorldMapElement WorldMap => ReadObjectAt<WorldMapElement>(0xCD8);
+		public WorldMapElement AreaInstanceUi => ReadObjectAt<WorldMapElement>(0x7C0);
 	    public IncursionWindow IncursionWindow => GameController.Instance.Game.IngameState.UIRoot.GetChildFromIndices(1, 58).AsObject<IncursionWindow>();
 
 		public IEnumerable<LabelOnGround> ItemsOnGroundLabels
 		{
 			get
 			{
-				var itemsOnGroundLabelRoot = GetObject<ItemsOnGroundLabelElement>(M.ReadLong(Address + 0x5A8));
+				var itemsOnGroundLabelRoot = GetObject<ItemsOnGroundLabelElement>(M.ReadLong(Address + 0x5C0));
 				return itemsOnGroundLabelRoot.LabelsOnGround;
 			}
 		}
-		public Element GemLvlUpPanel => ReadObjectAt<Element>(0xBA0);
-		public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0x928);//or 0x250
+		public Element GemLvlUpPanel => ReadObjectAt<Element>(0x8D8);
+		public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0x958);//or 0x250
 
 		//public bool IsDndEnabled => M.ReadByte(Address + 0xf92) == 1;
 		//public string DndMessage => M.ReadStringU(M.ReadLong(Address + 0xf98));
@@ -46,7 +46,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		{
 			get
 			{
-				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0x9F0));
+				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0xA08));
 				return stateListPres.Where(x => x.Item2 > 0).Select(x => new Tuple<Quest, int>(GameController.Instance.Files.Quests.GetByAddress(x.Item1), x.Item2)).ToList();
 			}
 		}
@@ -55,7 +55,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		{
 			get
 			{
-				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0x9F0));
+				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0xA08));
 				return stateListPres.Where(x => x.Item2 == 0).Select(x => new Tuple<Quest, int>(GameController.Instance.Files.Quests.GetByAddress(x.Item1), x.Item2)).ToList();
 			}
 		}
@@ -86,7 +86,7 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
 		{
 			get
 			{
-				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0x9F0));
+				var stateListPres = M.ReadDoublePointerIntList(M.ReadLong(Address + 0xA08));
 				return stateListPres.Select(x => new Tuple<Quest, int>(GameController.Instance.Files.Quests.GetByAddress(x.Item1), x.Item2)).ToList();
 			}
 		}
