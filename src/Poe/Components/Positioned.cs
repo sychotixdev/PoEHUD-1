7ff08d4c1d43455f6f886331dc99b10b830793f9
TehCheat <thecheatuc@gmail.com>
2019-07-09 12:10:25 -0400

    Fixed positioned offsets.

--- src/Poe/Components/Positioned.cs
+++ src/Poe/Components/Positioned.cs
@@ -6,17 +6,17 @@ namespace PoeHUD.Poe.Components
 
 	public class Positioned : Component
 	{
-		public int GridX => Address != 0 ? M.ReadInt(Address + 0x60) : 0;
-		public int GridY => Address != 0 ? M.ReadInt(Address + 0x64) : 0;
+		public int GridX => Address != 0 ? M.ReadInt(Address + 0xE8) : 0;
+		public int GridY => Address != 0 ? M.ReadInt(Address + 0xEC) : 0;
 		public Vector2 GridPos => new Vector2(GridX, GridY);
 
-		public float WorldX => M.ReadFloat(Address + 0xB8);
-		public float WorldY => M.ReadFloat(Address + 0xBC);
+		public float WorldX => M.ReadFloat(Address + 0x114);
+		public float WorldY => M.ReadFloat(Address + 0x118);
 		public Vector2 WorldPos => new Vector2(WorldX, WorldY);
 
 		public float Rotation => M.ReadFloat(Address + 0xF0); // Radians, btw
 		public float RotationDeg => Rotation * (180 / MathUtil.Pi);
 
-        public byte Reaction => M.ReadByte(Address + 0x72);
+        public byte Reaction => M.ReadByte(Address + 0x58);
 	}
 }
