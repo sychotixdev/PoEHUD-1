ec7b22067af6169e6d68e00561e794d00466026f
Sychotix <sychotixdev@gmail.com>
2019-08-28 14:48:09 -0400

    Corrected Chest offsets again, added a few freebies

--- src/Hud/EnumOffsets.cs
+++ src/Hud/EnumOffsets.cs
@@ -268,8 +268,10 @@ namespace PoeHUD.Hud
         [StructLayout(LayoutKind.Explicit)]
         public struct Chest
         {
-            [FieldOffset(0x74)] public readonly byte isOpened;
-            [FieldOffset(0xB4)] public readonly int isStrongbox;
+            [FieldOffset(0x78)] public readonly byte isOpened;
+            [FieldOffset(0x79)] public readonly byte isLocked;
+            [FieldOffset(0x7C)] public readonly byte quality;
+            [FieldOffset(0xB8)] public readonly int isStrongbox;
         }
 
         [StructLayout(LayoutKind.Explicit)]
